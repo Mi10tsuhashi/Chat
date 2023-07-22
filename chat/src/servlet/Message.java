ba1e1757ae2d806a089b2dc2cf91c6c82422d7ff
Mi10tsuhashi <43391227+Mi10tsuhashi@users.noreply.github.com>
2023-07-22 13:00:06 +0900

    Update Message.java
    コメントの追加。

--- chat/src/servlet/Message.java
+++ chat/src/servlet/Message.java
@@ -16,12 +16,19 @@ import beans.UserBean;
 import dao.MessageDAO;
 import dto.MessageDTO;
 
-
+/**
+*ログイン後のチャットデータの登録と、送信を担うサーブレット。
+*/
 @WebServlet("/message")
 public class Message extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
+/**
+*データがある場合は新規にメッセージを登録した後、コマンド処理を行い、最新のメッセージ群をJSON形式で返す。
+*データがない場合は最新のメッセージ群をJSON形式で返すのみで、Ajaxで定期的に呼ばれる。
+*@param request HTTPリクエスト。ここでは発言したメッセージ内容になる。
+*@param response HTTPレスポンス。JSON形式でチャットデータを返す。
+*/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		MessageDAO dao = new MessageDAO(this);
@@ -34,6 +41,12 @@ public class Message extends HttpServlet {
         dto.setDatetime(LocalDateTime.now());
         dto.setIP(request.getRemoteAddr());
         MessageDAO.insertMessageDTO(dto);
+/*
+*コマンドの処理。
+*チャット欄で　「/del」　と入力された場合は入力したユーザーが発言したメッセージを全て削除する。
+*「/delall」　と入力された場合はメッセージを全て削除する。
+*/
+
         if(text.startsWith("/del")&&!text.startsWith("/delall")) {
         dao.deleteMessage(((UserBean)request.getSession().getAttribute("LoginUser")).getName());
         }else if (text.startsWith("/delall")){
@@ -41,6 +54,7 @@ public class Message extends HttpServlet {
         }
         }
 		}
+		//データベースに登録されているメッセージから30件取得し、JSONで返す。
 		GsonBuilder builder = new GsonBuilder();
 		builder.registerTypeAdapter(MessageDTO.class, dto);
 		Gson gson = builder.create();
