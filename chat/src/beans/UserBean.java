5a41610350814b822c64dd9a9c2f124b2fc47278
Mi10tsuhashi <43391227+Mi10tsuhashi@users.noreply.github.com>
2023-07-22 10:03:41 +0900

    Update UserBean.java
    コメントの追加

--- chat/src/beans/UserBean.java
+++ chat/src/beans/UserBean.java
@@ -6,7 +6,9 @@ import java.util.TreeSet;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
-
+/**
+*現在ログインしているユーザーを表すJavaBean。
+*/
 public class UserBean implements Serializable , HttpSessionBindingListener , Comparable<UserBean>{
       private String name;
       private static final long serialVersionUID = 1L;
@@ -19,6 +21,11 @@ public class UserBean implements Serializable , HttpSessionBindingListener , Com
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+ 	*ログイン中のユーザーがログアウトし、セッションからバインド解除された時に、ログイン中の
+  	*ユーザー群を集合からも削除している。
+   	*@param event セッションを識別するイベント
+	*/
 	@Override
 	public void valueUnbound(HttpSessionBindingEvent event) {
        ServletContext servlet =event.getSession().getServletContext();
@@ -30,10 +37,16 @@ public class UserBean implements Serializable , HttpSessionBindingListener , Com
        userset.remove(bean);
        }
 	}
+	/**
+ 	*ユーザー名を元にハッシュコードを計算するようにオーバーライド
+	*/
 	@Override
 	public int hashCode() {
 		return this.name.hashCode();
 	}
+	/**
+ 	*ユーザー名を元に同値判定を行うようにオーバーライド
+	*/
 	@Override
 	public boolean equals(Object obj) {
 		if(!(obj instanceof UserBean)) {
@@ -41,6 +54,9 @@ public class UserBean implements Serializable , HttpSessionBindingListener , Com
 		}
 		return this.name.equals(((UserBean)obj).getName());
 	}
+	/**
+ 	*ユーザー名を元に比較するようにオーバーライド
+	*/
 	@Override
 	public int compareTo(UserBean o) {
 
